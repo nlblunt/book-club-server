dae5582c61b57e4205cdb66218dcea6a59160204
Nicolas Willis-Blunt <nicolas.blunt@gmail.com>
2015-10-31 23:53:51 +0000

    Change DB: Books -> Add string: google_id

--- db/migrate/20151031061727_create_books.rb
+++ db/migrate/20151031061727_create_books.rb
@@ -1,6 +1,7 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
+      t.string :google_id
       t.string :title
       t.text :description
       t.string :author
